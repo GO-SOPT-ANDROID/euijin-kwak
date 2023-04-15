c692db16f2bb484d693cacbf43443aa66e9430db
KwakEuiJin <yuijin0909@gmail.com>
2023-04-15 14:18:54 +0900

    feat(semina2): ConcatAdapter 추가 및 완료

---
+++ app/src/main/java/org/android/go/sopt/util/MusicList.kt
@@ -0,0 +1,57 @@
+package org.android.go.sopt.util
+
+import org.android.go.sopt.model.MusicData
+
+object MusicList {
+    val musicList = listOf(
+        MusicData(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+            "좋은 날",
+            "아이유"
+        ),
+        MusicData(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+            "좋은 날",
+            "아이유"
+        ),
+        MusicData(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+            "좋은 날",
+            "아이유"
+        ),
+        MusicData(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+            "좋은 날",
+            "아이유"
+        ),
+        MusicData(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+            "좋은 날",
+            "아이유"
+        ),
+        MusicData(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+            "좋은 날",
+            "아이유"
+        ),
+        MusicData(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+            "좋은 날",
+            "아이유"
+        ),
+        MusicData(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+            "좋은 날",
+            "아이유"
+        ),
+        MusicData(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+            "좋은 날",
+            "아이유"
+        ),
+        MusicData(
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+            "좋은 날",
+            "아이유")
+    )
+}
