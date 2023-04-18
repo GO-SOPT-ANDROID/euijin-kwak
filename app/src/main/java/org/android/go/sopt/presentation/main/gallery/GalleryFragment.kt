6962db0658a46180d69689932113aa8b21b12284
KwakEuiJin <yuijin0909@gmail.com>
2023-04-19 00:22:40 +0900

    feat(semina2): Fake URL 연동 및 kotlinx-serialization 파싱, 깃 레포 recyclerView 구성

--- app/src/main/java/org/android/go/sopt/presentation/main/gallery/GalleryFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/main/gallery/GalleryFragment.kt
@@ -1,16 +1,32 @@
 package org.android.go.sopt.presentation.main.gallery
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import coil.load
+import coil.transform.CircleCropTransformation
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.android.go.sopt.databinding.FragmentGalleryBinding
-import org.android.go.sopt.databinding.FragmentSearchBinding
+import org.android.go.sopt.model.FakeRepoResponse
+import org.android.go.sopt.presentation.main.gallery.github.GitRepoAdapter
 
-class GalleryFragment: Fragment() {
+class GalleryFragment : Fragment() {
     private var _binding: FragmentGalleryBinding? = null
     private val binding get() = _binding!!
+
+    private val gitRepoAdapter by lazy {
+        GitRepoAdapter { url ->
+            Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+                startActivity(this)
+            }
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
@@ -19,10 +35,40 @@ class GalleryFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val data = parseData()
+        initViews(data)
+        initAdapter(data)
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    private fun parseData(): List<FakeRepoResponse.FakeRepoResponseItem> {
+        val jsonString = requireContext().assets.open("fake_repo_list.json").reader().readText()
+        return parseJson(jsonString)
+    }
+
+    private fun initViews(data: List<FakeRepoResponse.FakeRepoResponseItem>) {
+        with(binding) {
+            val userData = data.first().owner
+            tvName.text = userData.login
+            ivProfile.load(userData.avatar_url) {
+                transformations(CircleCropTransformation())
+            }
+        }
+    }
+
+    private fun initAdapter(data: List<FakeRepoResponse.FakeRepoResponseItem>) {
+        binding.rvRepoList.apply {
+            adapter = gitRepoAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        gitRepoAdapter.submitList(data)
+    }
+
+    private fun parseJson(jsonString: String): List<FakeRepoResponse.FakeRepoResponseItem> {
+        return Json.decodeFromString(jsonString)
     }
 }
---
+++ app/src/main/java/org/android/go/sopt/presentation/main/gallery/github/GitRepoAdapter.kt
@@ -0,0 +1,53 @@
+package org.android.go.sopt.presentation.main.gallery.github
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import org.android.go.sopt.R
+import org.android.go.sopt.databinding.ItemGitRepoBinding
+import org.android.go.sopt.model.FakeRepoResponse
+
+
+class GitRepoAdapter(private val onClick: (String) -> Unit) : ListAdapter<FakeRepoResponse.FakeRepoResponseItem, GitRepoAdapter.GitRepoViewHolder>(diffUtil) {
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<FakeRepoResponse.FakeRepoResponseItem>() {
+            override fun areItemsTheSame(oldItem: FakeRepoResponse.FakeRepoResponseItem, newItem: FakeRepoResponse.FakeRepoResponseItem): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: FakeRepoResponse.FakeRepoResponseItem, newItem: FakeRepoResponse.FakeRepoResponseItem): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GitRepoViewHolder {
+        return GitRepoViewHolder(ItemGitRepoBinding.inflate(LayoutInflater.from(parent.context), parent, false), onClick)
+    }
+
+    override fun onBindViewHolder(holder: GitRepoViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    class GitRepoViewHolder(private val binding: ItemGitRepoBinding, private val onClick: (String) -> Unit) : ViewHolder(binding.root) {
+        fun bind(fakeRepoResponseItem: FakeRepoResponse.FakeRepoResponseItem) {
+            with(binding) {
+                root.context.let { context ->
+                    tvRepoName.text = fakeRepoResponseItem.name
+                    tvRepoDescription.text = fakeRepoResponseItem.description
+                    tvOwner.text = context.getString(R.string.owner, fakeRepoResponseItem.owner.login)
+                    tvRepoLanguage.text = context.getString(R.string.language, fakeRepoResponseItem.language)
+                    tvRepoStar.text = context.getString(R.string.stars, fakeRepoResponseItem.stargazers_count.toString())
+                    tvRepoFork.text = context.getString(R.string.forks, fakeRepoResponseItem.forks_count.toString())
+                }
+            }
+            itemView.setOnClickListener {
+                onClick(fakeRepoResponseItem.html_url)
+            }
+        }
+    }
+
+}
